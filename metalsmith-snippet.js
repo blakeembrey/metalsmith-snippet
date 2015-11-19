bd96e796d044f38f5775379ba55d39956f0c2f3d
Blake Embrey <hello@blakeembrey.com>
2015-11-18 22:15:59 -0800

    Refactor API for v2 rewrite

--- metalsmith-snippet.js
+++ metalsmith-snippet.js
@@ -1,115 +1,209 @@
-var extend   = require('extend');
-var extname  = require('path').extname;
-var entities = require('ent');
+var he = require('he')
+var extend = require('xtend')
+var arrify = require('arrify')
+var voidElements = require('void-elements')
+var extname = require('path').extname
+
+/**
+ * Expose snippet function.
+ */
+module.exports = metalsmithSnippet
+
+/**
+ * Default options.
+ */
+var DEFAULTS = {
+  suffix: '…',
+  maxLength: Infinity,
+  stripHtml: true,
+  stripPre: true
+}
+
+/**
+ * Match trailing punctuation.
+ */
+var PUNCTUATION_REGEXP = /[\s,\.!?]/
+var TRAILING_PUNCTUATION_REGEXP = /[\s,\.!?]+$/
+var WHITESPACE_REGEXP = /\s/
 
 /**
  * Support creating excerpts from basic html.
  *
- * @param  {Object}   maxLength
+ * @param  {Object}               opts
+ * @param  {number}               [opts.maxLength]
+ * @param  {string|Array<string>} [opts.stop]
+ * @param  {boolean}              [opts.stripHtml]
+ * @param  {boolean}              [opts.stripPre]
  * @return {Function}
  */
-module.exports = function (opts) {
-  opts = extend({
-    suffix: '…',
-    maxLength: 300,
-    stopAtSubstring: false,
-    stops: ['<span class="more', '<h2', '<hr']
-  }, opts);
+function metalsmithSnippet (opts) {
+  // Always `arrify` the `stop` option.
+  var options = extend(DEFAULTS, opts)
+
+  // Sanitize data input.
+  options.stop = arrify(options.stop)
+  options.maxLength = +options.maxLength || Infinity
+
+  // Verify the options are correctly set.
+  if (options.maxLength === Infinity && options.stop.length === 0) {
+    throw new TypeError('Metalsmith snippet needs `maxLength` or `stop` to be set to function properly')
+  }
 
   return function (files, metalsmith, done) {
     Object.keys(files).forEach(function (file) {
-      var data = files[file];
+      var data = files[file]
 
-      // Skip attaching a snippet if one has been defined already.
-      if (data.snippet || !isHtml(file)) {
-        return;
+      // Skip attaching a snippet when one has been defined.
+      if (data.snippet != null || !isHtml(file)) {
+        return
       }
 
-      data.snippet = extractSnippet(data.contents.toString(), opts);
-    });
+      data.snippet = extractSnippet(data.contents.toString(), options)
+    })
 
-    return done();
-  };
-};
+    return done()
+  }
+}
 
 /**
  * Extract a short snippet based on the file contents.
  *
  * @param  {String} contents
- * @param  {Object} opts
+ * @param  {Object} options
  * @return {String}
  */
-function extractSnippet (contents, opts) {
-  if (opts.stopAtSubstring) {
-    return trimToSubstring(contents, opts);
-  }
-  else {
-    return trimToLength(contents, opts);
-  }
-}
+function extractSnippet (contents, options) {
+  var hasMore = false
+  var suffix = he.encode(options.suffix)
 
-/**
- * Trims to a specified maxLength with optional suffix
- *
- * @param  {String} contents
- * @param  {Object} opts
- * @return {String}
- */
-function trimToLength (contents, opts) {
-  // Remove code sections, html tags and any additional whitespace.
-  contents = entities.decode(contents)
-    .replace(/<pre[^>]*>[\s\S]*?<\/pre>/g, '')
-    .replace(/<[^>]+>/g, '')
-    .replace(/\s+/, ' ')
-    .trim();
-
-  // If the total contents length is already shorted, return.
-  if (contents.length < opts.maxLength) {
-    return entities.encode(contents);
+  // Replace `pre` sections when specified, avoids counting characters in code.
+  if (options.trimPre) {
+    contents = contents.replace(/<pre[^>]*>[\s\S]*?<\/pre>/g, '')
   }
 
-  // Find the last whitespace break before the maximum length.
-  var nonWordIndex = contents.substr(0, opts.maxLength + 1).lastIndexOf(' ');
+  // If a stop occurs before `maxLength`, use that.
+  for (var i = 0; i < options.stop.length; i++) {
+    var stopIndex = contents.indexOf(options.stop[i])
 
-  // A non-word index could not be found within the `maxLength`, return nothing.
-  if (nonWordIndex === -1) {
-    return '';
+    if (stopIndex > -1) {
+      hasMore = true
+      contents = contents.substr(0, stopIndex)
+    }
   }
 
-  // Extract the string using the maximum number of words within the length. We
-  // also trim trailing punctuation characters.
-  contents = contents.substr(0, nonWordIndex).replace(/[,\.!?;]+$/, '');
+  // Stop HTML tags as I "parse" them.
+  var tags = []
+  var isInTag = false
+  var length = 0
+  var contentBreak = -1
+  var whitespaceBreak = -1
 
-  return entities.encode(contents + opts.suffix);
-}
+  // Traverse the HTML, should be stable will well written HTML.
+  for (var offset = 0; offset < contents.length; offset++) {
+    var char = contents.charAt(offset)
+    var nextChar = contents.charAt(offset + 1)
 
-/**
- * Trims to the content before a specified substring or array of substrings
- *
- * @param  {String} contents
- * @param  {Object} opts
- * @return {String}
- */
-function trimToSubstring (contents, opts) {
-    contents = contents.trim();
+    if (char === '<') {
+      var tagname = ''
+      var isClosingTag = nextChar === '/'
 
-    // Make sure stops is an Array
-    if (opts.stops.constructor !== Array) {
-      opts.stops = [opts.stops];
+      // Increment the parser after closing tag.
+      if (isClosingTag) {
+        offset++
+      }
+
+      // Set "isInTag" to `true`.
+      isInTag = true
+
+      while (offset++ < contents.length) {
+        // Stop parsing tag name on first white space or closing tag.
+        if (/\s|>/.test(contents[offset])) {
+          isInTag = contents[offset] !== '>'
+
+          if (tagname) {
+            if (isClosingTag) {
+              if (tags[tags.length - 1] === tagname) {
+                tags.pop()
+              }
+            } else {
+              if (!voidElements.hasOwnProperty(tagname)) {
+                tags.push(tagname)
+              }
+            }
+          }
+
+          break
+        }
+
+        tagname += contents[offset]
+      }
+    } else if (char === '>') {
+      isInTag = false
+    } else if (char === '&') {
+      for (var j = offset; j < contents.length; j++) {
+        if (WHITESPACE_REGEXP.test(contents[j])) {
+          break
+        } else if (contents[j] === ';') {
+          length++
+          offset = j
+          break
+        }
+      }
+    } else if (!isInTag) {
+      // Keep track of the HTML break.
+      if (WHITESPACE_REGEXP.test(contents[offset])) {
+        whitespaceBreak = offset
+
+        // Ignore additional whitespace in length calculations.
+        while (WHITESPACE_REGEXP.test(contents[offset + 1])) {
+          offset++
+        }
+      }
+
+      length++
     }
 
-    var idx = Infinity;
-    // Find the earliest stopping point
-    opts.stops.forEach(function (stop) {
-      var i = contents.indexOf(stop);
-      if (i !== -1 && i < idx) {
-        idx = i;
-      }
-    });
+    if (length >= options.maxLength) {
+      hasMore = true
+      contentBreak = offset
 
-    // Don't add the suffix or trim trailing characters.
-    // The content has already decided where it wants to stop.
-    return idx !== Infinity ? contents.substr(0, idx) : contents;
+      // Check the following character for punctuation.
+      if (PUNCTUATION_REGEXP.test(contents[offset + 1])) {
+        whitespaceBreak = offset + 1
+      }
+
+      break
+    }
+  }
+
+  // Ignore snippets when a whitespace break was not found.
+  if (whitespaceBreak === -1) {
+    return ''
+  }
+
+  // Remove content past the `maxLength`.
+  if (contentBreak > -1) {
+    contents = contents.substr(0, whitespaceBreak)
+  }
+
+  // Strip or correct invalid HTML snippet.
+  if (options.stripHtml) {
+    contents = contents.replace(/<\S+?[^>]*>/g, '').replace(/\s+/g, ' ')
+  }
+
+  // Append suffix to contents.
+  if (hasMore) {
+    contents = contents.replace(TRAILING_PUNCTUATION_REGEXP, '') + suffix
+  }
+
+  if (!options.stripHtml) {
+    // Pop off in reverse order to output valid HTML.
+    while (tags.length) {
+      contents += '</' + tags.pop() + '>'
+    }
+  }
+
+  return contents
 }
 
 /**
@@ -119,5 +213,5 @@ function trimToSubstring (contents, opts) {
  * @return {Boolean}
  */
 function isHtml (file) {
-  return extname(file) === '.html';
+  return extname(file) === '.html'
 }
--- test.js
+++ test.js
@@ -1,154 +1,252 @@
-var expect  = require('chai').expect;
-var snippet = require('./');
+/* global describe, it */
+
+var expect = require('chai').expect
+var snippet = require('./')
 
 describe('metalsmith snippet', function () {
   describe('extract all text when length is shorter than max', function () {
     var files = {
-      'test.html': { contents: 'Hello, world!' }
-    };
+      'test.html': { contents: '<p>Hello, world!</p>' }
+    }
 
     it('should extract the snippet', function (done) {
-      return snippet()(files, {}, function (err) {
-        expect(files['test.html'].snippet).to.equal('Hello, world!');
+      return snippet({ maxLength: 500 })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal('Hello, world!')
 
-        return done(err);
-      });
-    });
-  });
+        return done(err)
+      })
+    })
+  })
 
   describe('should not override existing snippets', function () {
     var files = {
       'test.html': {
         contents: 'Hello, world!',
-        snippet: 'Look ma, I added my own snippet!'
+        snippet: 'Existing snippet...'
       }
-    };
+    }
 
     it('should skip extracting a snippet', function (done) {
-      return snippet()(files, {}, function (err) {
+      return snippet({ stop: ['<span'] })(files, {}, function (err) {
         expect(files['test.html'].snippet).to.equal(
-          'Look ma, I added my own snippet!'
-        );
+          'Existing snippet...'
+        )
 
-        return done(err);
-      });
-    });
-  });
+        return done(err)
+      })
+    })
+  })
 
   describe('no word breaks', function () {
     var files = {
       'test.html': {
         contents: 'Supercalifragilisticexpialidocious'
       }
-    };
+    }
 
     it('should not set the snippet when it does not fit', function (done) {
       return snippet({ maxLength: 20 })(files, {}, function (err) {
-        expect(files['test.html'].snippet).to.be.empty;
+        expect(files['test.html'].snippet).to.be.empty
 
-        return done(err);
+        return done(err)
       })
-    });
-  });
+    })
+  })
 
   describe('break between words', function () {
     var files = {
       'test.html': {
         contents: 'This is some simple test content.'
       }
-    };
+    }
 
     it('should break on whitespace before the max length', function (done) {
       return snippet({ maxLength: 20 })(files, {}, function (err) {
         expect(files['test.html'].snippet).to.equal(
-          'This is some simple&#8230;'
-        );
+          'This is some simple&#x2026;'
+        )
 
-        return done(err);
-      });
-    });
-  });
+        return done(err)
+      })
+    })
+  })
+
+  describe('ignore void elements', function () {
+    var files = {
+      'test.html': {
+        contents: '<p>This is a <img src="foo.png"> test break.</p>'
+      }
+    }
+
+    it('should break before max length', function (done) {
+      return snippet({ maxLength: 15 })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal(
+          'This is a test&#x2026;'
+        )
+
+        return done(err)
+      })
+    })
+  })
+
+  describe('do not count html entities', function () {
+    var files = {
+      'test.html': {
+        contents: 'Super simple&mdash;test script.'
+      }
+    }
+
+    it('should break before max length', function (done) {
+      return snippet({ maxLength: 20 })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal(
+          'Super simple&mdash;test&#x2026;'
+        )
+
+        return done(err)
+      })
+    })
+  })
 
   describe('trim punctuation before break', function () {
     var files = {
       'test.html': {
         contents: 'God, damn, commas.'
       }
-    };
+    }
 
     it('should trim the comma before appending the suffix', function (done) {
       return snippet({ maxLength: 10 })(files, {}, function (err) {
-        expect(files['test.html'].snippet).to.equal('God, damn&#8230;');
+        expect(files['test.html'].snippet).to.equal('God, damn&#x2026;')
 
-        return done(err);
-      });
-    });
-  });
+        return done(err)
+      })
+    })
+  })
 
   describe('custom suffix', function () {
     var files = {
       'test.html': {
         contents: 'Hello, world!'
       }
-    };
+    }
 
     it('should be able to pass in a custom suffix', function (done) {
       return snippet({
         maxLength: 8,
         suffix: '...'
       })(files, {}, function (err) {
-        expect(files['test.html'].snippet).to.equal('Hello...');
+        expect(files['test.html'].snippet).to.equal('Hello...')
 
-        return done(err);
-      });
-    });
-  });
+        return done(err)
+      })
+    })
+  })
 
-  describe('stop at substring default', function () {
+  describe('stop', function () {
     var files = {
       'test.html': {
-        contents: 'Hello, world!<span class="more"></span> This stops before printing this.'
+        contents: 'Hello,   world!<span class="more"></span> This stops before printing this.'
       }
-    };
+    }
 
     it('should trim to before the span', function (done) {
-      return snippet({ stopAtSubstring: true })(files, {}, function (err) {
-        expect(files['test.html'].snippet).to.equal('Hello, world!');
+      return snippet({ stop: '<span class="more"' })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal('Hello, world&#x2026;')
 
-        return done(err);
-      });
-    });
-  });
+        return done(err)
+      })
+    })
+  })
 
-  describe('stop at earliest substring', function () {
+  describe('multiple stops', function () {
     var files = {
       'test.html': {
         contents: 'Hello, <hr>world! <span class="more"></span>This stops after Hello, '
       }
-    };
+    }
 
-    it('should trim to "Hello, "', function (done) {
-      return snippet({ stopAtSubstring: true })(files, {}, function (err) {
-        expect(files['test.html'].snippet).to.equal('Hello, ');
+    it('should trim to first stop index', function (done) {
+      return snippet({ stop: ['<span class="more"', '<hr'] })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal('Hello&#x2026;')
 
-        return done(err);
-      });
-    });
-  });
+        return done(err)
+      })
+    })
+  })
 
-  describe('should allow custom stops', function () {
+  describe('append unclosed html tags', function () {
+    var files = {
+      'test.html': {
+        contents: '<p>Hello, <hr>world!</p>'
+      }
+    }
+
+    it('should trim to first stop index', function (done) {
+      return snippet({
+        stop: ['<hr'],
+        stripHtml: false
+      })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal('<p>Hello&#x2026;</p>')
+
+        return done(err)
+      })
+    })
+  })
+
+  describe('custom stop', function () {
     var files = {
       'test.html': {
         contents: 'Hello, world! <span class="more"></span>This will print some of this sentence.'
       }
-    };
+    }
 
-    it('should trim to before the word "some"', function (done) {
-      return snippet({ stopAtSubstring: true, stops: ['some']})(files, {}, function (err) {
-        expect(files['test.html'].snippet).to.equal('Hello, world! <span class="more"></span>This will print ');
+    it('should trim to custom stop index', function (done) {
+      return snippet({ stop: 'some' })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal(
+          'Hello, world! This will print&#x2026;'
+        )
 
-        return done(err);
-      });
-    });
-  });
-});
+        return done(err)
+      })
+    })
+  })
+
+  describe('skip not found stop token', function () {
+    var files = {
+      'test.html': {
+        contents: 'This will print everything.'
+      }
+    }
+
+    it('should trim to custom stop index', function (done) {
+      return snippet({
+        stop: 'foo'
+      })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal('This will print everything.')
+
+        return done(err)
+      })
+    })
+  })
+
+  describe('should not strip html', function () {
+    var files = {
+      'test.html': {
+        contents: 'Hello, world! <span class="more"></span>This will print some of this sentence.'
+      }
+    }
+
+    it('should trim to custom stop index', function (done) {
+      return snippet({
+        stop: 'some',
+        stripHtml: false
+      })(files, {}, function (err) {
+        expect(files['test.html'].snippet).to.equal(
+          'Hello, world! <span class="more"></span>This will print&#x2026;'
+        )
+
+        return done(err)
+      })
+    })
+  })
+})
